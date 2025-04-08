5911573b8cefadf2486850251915dd285ea91663
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-04-08 03:02:37 +0200

    feat : create profile component

--- src/app/home/components/profile/profile.component.ts
+++ src/app/home/components/profile/profile.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
-  standalone:false
+  standalone: false,
 })
-export class ProfileComponent  implements OnInit {
+export class ProfileComponent implements OnInit {
+  profileForm!: FormGroup;
+  fb = inject(FormBuilder);
+  isEditing: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {
+    this.profileForm = this.fb.group({
+      email: ['m.osamasasdfasfas@gmail.com'],
+      phone: ['+201143687888'],
+      gender: ['Male'],
+    });
+    this.profileForm.disable();
+  }
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
 
-  ngOnInit() {}
-
+    if (this.isEditing) {
+      this.profileForm.enable();
+    } else {
+      this.profileForm.disable();
+    }
+  }
 }
